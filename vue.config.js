74c7ee198eba28edf01dc3a7c4948390b5e80a50
liuying <wocanbe@163.com>
2020-09-10 22:12:54 +0800

    添加brotli压缩算法

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const path = require("path")
 const CompressionPlugin = require("compression-webpack-plugin")
+const zopfli = require("@gfx/zopfli")
+const BrotliPlugin = require("brotli-webpack-plugin")
 
 const isProduction = process.env.NODE_ENV === 'production'
+const prodGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -39,22 +42,23 @@ module.exports = {
     if (isProduction) {
       config.plugins.push(
         new CompressionPlugin({
-          /* [file]被替换为原始资产文件名。
-             [path]替换为原始资产的路径。
-             [dir]替换为原始资产的目录。
-             [name]被替换为原始资产的文件名。
-             [ext]替换为原始资产的扩展名。
-             [query]被查询替换。*/
-          filename: '[path].gz[query]',
-          //压缩算法
-          algorithm: 'gzip',
-          //匹配文件
-          test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
+          algorithm(input, compressionOptions, callback) {
+            return zopfli.gzip(input, compressionOptions, callback);
+          },
+          compressionOptions: {
+            numiterations: 15
+          },
           //压缩超过此大小的文件,以字节为单位
-          threshold: 10240,
+          threshold: 51200, // 50KB
           minRatio: 0.8,
-          //删除原始文件只保留压缩后的文件
-          // deleteOriginalAssets: true
+          test: prodGzipExtensions
+        })
+      )
+      config.plugins.push(
+        new BrotliPlugin({
+          test: prodGzipExtensions,
+          threshold: 51200,
+          minRatio: 0.8
         })
       )
     }
